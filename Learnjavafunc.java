5502e62c40f915305ce95f7c7023834c3c68dcd2
Ansh varma <122360981+anshvrma@users.noreply.github.com>
2025-12-15 18:14:30 +0530

    getDecimalToAnyBase

--- Learnjavafunc.java
+++ Learnjavafunc.java
@@ -1,27 +1,79 @@
 import java.util.Scanner;
 
 public class Learnjavafunc {
-    public static int fact(int x) {
-        int rv = 1;
+    // (npr function- permutaion and combinaton)
+    // public static int fact(int x) {
+    //     int rv = 1;
 
-        for(int i = 1; i <= x; i++) {
-            rv = rv * i;
-        }
+    //     for(int i = 1; i <= x; i++) {
+    //         rv = rv * i;
+    //     }
 
-        return rv;
-    }
+    //     return rv;
+    // }
 
-    public static void main(String[] args) {
-        Scanner scn_num = new Scanner(System.in);
-        int n = scn_num.nextInt();
-        int r = scn_num.nextInt();
+    // public static void main(String[] args) {
+    //     Scanner scn_num = new Scanner(System.in);
+    //     int n = scn_num.nextInt();
+    //     int r = scn_num.nextInt();
 
-        int nfact = fact(n);
-        int nmrfact = fact(n - r);
+    //     int nfact = fact(n);
+    //     int nmrfact = fact(n - r);
 
-        int npr = nfact / nmrfact;
-        System.out.println(n + "P" + r + " = " + npr); 
-    }
+    //     int npr = nfact / nmrfact;
+    //     System.out.println(n + "P" + r + " = " + npr); 
+    // }
+
+    // Digit counter
+    // public static int digi_count(int n, int x) {
+    //     int count = 0;
+    //     int div = 0;
+
+    //     while(n > 0){
+    //         div = n % 10;
+    //         n = n/10;
+    //         if(div == x){
+    //             count++;
+    //         }
+    //     }
+    //     return count;
+    // }
+
+    // public static void main(String[] args) {
+    //     Scanner scn = new Scanner(System.in);
+    //     int n = scn.nextInt();
+    //     int x = scn.nextInt();
+
+    //     int dig_num = digi_count(n, x);
+    //     System.out.println(dig_num);
+    // }
+
+    // DEcimal to any base
+    // public static void main(String[] args) {
+    //     Scanner scn = new Scanner(System.in);
+    //     System.out.print("Enter the Digit of number you want to covert: ");
+    //     int n = scn.nextInt();
+    //     System.out.println("Enter the number of Base: ");
+    //     int b = scn.nextInt();
+    //     int dn = getInBase(n, b);
+    //     System.out.println(dn);
+    // }
+    // public static int getInBase(int n, int b){
+    //     int rv = 0;
+    //     int p = 1;
+    //     while(n > 0) {
+    //         int dig = n % b;
+    //         n = n/b;
+    //         rv += dig * p;
+    //         p = p * 10;
+    //     }
+
+    //     return rv;
+    // }
+
+    
+
+
 
 
 
