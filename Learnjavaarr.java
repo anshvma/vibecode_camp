ae85853651e480386975800390bf7f46d131801f
Ansh varma <122360981+anshvrma@users.noreply.github.com>
2026-01-05 17:13:23 +0530

    span of an array

--- Learnjavaarr.java
+++ Learnjavaarr.java
@@ -4,20 +4,47 @@ import java.util.*;
 
 
 public class Learnjavaarr {
-    public static void main(String[] args) {
-        int[] arr;
+    // public static void main(String[] args) {
+    //     int[] arr;
 
-        arr = new int[5];
-        arr[0] = 24;
-        arr[1] = 59;
-        arr[2] = 37;
-        arr[3] = 61;
-        arr[4] = 95;
+    //     arr = new int[5];
+    //     arr[0] = 24;
+    //     arr[1] = 59;
+    //     arr[2] = 37;
+    //     arr[3] = 61;
+    //     arr[4] = 95;
 
-        System.out.println(arr.length);
-        for(int i = 0; i < arr.length; i++){
-            System.out.println(arr[i]);
-        }
+    //     System.out.println(arr.length);
+    //     for(int i = 0; i < arr.length; i++){
+    //         System.out.println(arr[i]);
+    //     }
 
-    }
+    // }
+
+    //span of an array
+
+    // public static void main(String[] args) {
+    //     Scanner scn = new Scanner(System.in);
+    //     int n = scn.nextInt();
+    //     int[] arr = new int[n];
+
+    //     for(int i = 0; i < arr.length; i++){
+    //         arr[i] = scn.nextInt();
+    //     }
+
+    //     int max = arr[0];
+    //     int min = arr[0];
+    //     for(int i = 1; i < arr.length; i++){
+    //         if(arr[i] > max){
+    //             max = arr[i];
+    //         }
+
+    //         if(arr[i] < min){
+    //             min = arr[i];
+    //         }
+    //     }
+
+    //     int span = max - min;
+    //     System.out.println(span);
+    // }
 }
