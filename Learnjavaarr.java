171d4e2bab9515887159f47c224b4afcc72c4b70
Ansh varma <122360981+anshvrma@users.noreply.github.com>
2026-01-06 14:11:18 +0530

    findingElementarr

--- Learnjavaarr.java
+++ Learnjavaarr.java
@@ -47,4 +47,33 @@ public class Learnjavaarr {
     //     int span = max - min;
     //     System.out.println(span);
     // }
+
+    //Find element in the array
+    // public static void main(String[] args) {
+    //     Scanner scn = new Scanner(System.in);
+    //     int n = scn.nextInt();
+    //     int[] arr = new int[n];
+
+    //     for(int i = 0; i < arr.length; i++){
+    //         arr[i] = scn.nextInt();
+    //     }
+
+    //     int find = scn.nextInt();
+    //     int idx = -1;
+
+    //     for(int i = 1; i < arr.length; i++){
+    //         if(find == arr[i]){
+    //             idx = i;
+    //             break;
+    //         }
+    //     }
+
+    //     System.out.println(idx);
+    // }
+
+    
+
+
+
+
 }
