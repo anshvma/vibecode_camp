7614b0d9162209a2f8733034d107491d534312ea
Ansh varma <122360981+anshvrma@users.noreply.github.com>
2026-01-08 17:23:51 +0530

    Sum2arr

--- Learnjavaarr.java
+++ Learnjavaarr.java
@@ -100,6 +100,56 @@ public class Learnjavaarr {
     //     }
     // }
 
+    //Sum of two array
+    // public static void main(String[] args) {
+    //     Scanner scn = new Scanner(System.in);
+    //     int n = scn.nextInt();
+    //     int[] arr = new int[n];
+    //     for(int i = 0; i < arr.length; i++){
+    //         arr[i] = scn.nextInt();
+    //     }
+    //     int n2 = scn.nextInt();
+    //     int[] arr2 = new int[n2];
+    //     for(int i = 0; i < arr2.length; i++){
+    //         arr2[i] = scn.nextInt();
+    //     }
+
+    //     int[] sum = new int[n > n2? n: n2];
+    //     int c = 0;
+
+    //     int i = arr.length - 1;
+    //     int j = arr2.length - 1;
+    //     int k = sum.length - 1;
+        
+    //     while (k >= 0){
+    //         int d = c;
+
+    //         if(i >= 0){
+    //             d += arr[i];
+    //         }
+    //         if(j >= 0){
+    //             d += arr2[j];
+    //         }
+            
+    //         c = d / 10;
+    //         d = d% 10;
+            
+    //         sum[k] = d;
+
+    //         i--;
+    //         j--;
+    //         k--;
+    //     }
+    //     if(c != 0){
+    //             System.out.print(c);
+    //         }
+
+    //     for(int val: sum){
+    //         System.out.print(val);
+    //     }
+    // }
+
+    
 
 
 
