037d2034c83735450164c8fc9dba430c8aeb3394
Ansh varma <122360981+anshvrma@users.noreply.github.com>
2026-01-12 16:19:14 +0530

    Intro2Stack

---
+++ LearnJava/Learnjavastk.java
@@ -0,0 +1,20 @@
+package LearnJava;
+
+import java.util.*;
+import java.io.*;
+
+public class Learnjavastk {
+    //Introduction to Stack
+    // public static void main(String[] args) {
+    //     Stack<Integer> st = new Stack<>();
+
+    //     st.push(10);
+    //     st.push(20);
+    //     System.out.println(st);
+    //     st.push(30);
+    //     System.out.println(st);
+    //     st.push(40);
+    //     System.out.println(st);
+    //     System.out.println(st.peek() + " " + st.size());
+    // }
+}
