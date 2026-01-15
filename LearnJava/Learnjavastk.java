113c9521317aafb12b0aab01371198a8e1df8ffb
Ansh varma <122360981+anshvrma@users.noreply.github.com>
2026-01-15 22:20:02 +0530

    balanceBrackets

--- LearnJava/Learnjavastk.java
+++ LearnJava/Learnjavastk.java
@@ -51,8 +51,54 @@ public class Learnjavastk {
     //     System.out.println(false);
     // }
 
+    // Balance Brackets
+    // public static void main(String[] args) {
+    //     Scanner scn = new Scanner(System.in);
+    //     String str = scn.nextLine();
+
+    //     Stack<Character> st = new Stack<>();
+    //     for(int i = 0; i < str.length(); i++){
+    //         char ch = str.charAt(i);
+    //         if(ch == '(' || ch == '{' || ch == '['){
+    //             st.push(ch);
+    //         } else if (ch == ')') {
+    //             boolean val = handleClosing(st, '(');
+    //             if(val == false){
+    //                 System.out.println(val);
+    //                 return;
+    //             }
+    //         } else if (ch == '}') {
+    //             boolean val = handleClosing(st, '{');
+    //             if(val == false){
+    //                 System.out.println(val);
+    //                 return;
+    //             }
+    //         } else if (ch == ']') {
+    //             boolean val = handleClosing(st, '[');
+    //             if(val == false){
+    //                 System.out.println(val);
+    //                 return;
+    //             }
+    //         }
+    //     }
+    //     if(st.size() != 0){
+    //         System.out.println(false);
+    //     } else {
+    //         System.out.println(true);
+    //     }
+    // }
     
+    // public static boolean handleClosing(Stack<Character> st, char corroch){
+    //     if(st.size() == 0) {
+    //         return false;
+    //     } else if(st.peek() != corroch){
+    //         return false;
+    //     } else {
+    //         st.pop();
+    //         return true;
+    //     }
+    // }
 
-
+    
 
 }
