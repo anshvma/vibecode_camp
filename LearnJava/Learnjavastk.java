e7cd109ee9eec9e243f3b10aeb29aa7b2e89e777
Ansh varma <122360981+anshvrma@users.noreply.github.com>
2026-01-18 15:53:59 +0530

    nge(altSol)

--- LearnJava/Learnjavastk.java
+++ LearnJava/Learnjavastk.java
@@ -146,8 +146,33 @@ public class Learnjavastk {
     //     return nge;
     // }
 
+    // //Alternative approach
+    // public static int[] solve(int[] arr){
+    //     int[] nge = new int[arr.length];
+
+    //     Stack<Integer> st = new Stack<>();
+
+    //     st.push(0);
+    //     for(int i= 1; i < arr.length; i++){
+    //         while(st.size() > 0 && arr[i] > arr[st.peek()]){
+    //             int pos = st.peek();
+    //             nge[pos] = arr[i];
+    //             st.pop();
+    //         }
+
+    //         st.push(i);
+    //     }
+
+    //     while(st.size() > 0){
+    //         int pos = st.peek();
+    //         nge[pos] = -1;
+    //         st.pop();
+    //     }
+    //     return nge;
+    // }
     
 
+    
 
 
 }
