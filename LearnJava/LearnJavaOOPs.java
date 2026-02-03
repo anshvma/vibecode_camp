907b9c3e4b0436a7e1a69564665459607afba97d
Ansh varma <122360981+anshvrma@users.noreply.github.com>
2026-02-03 16:47:17 +0530

    IntroOOPs

---
+++ LearnJava/LearnJavaOOPs.java
@@ -0,0 +1,34 @@
+package LearnJava;
+
+import java.util.*;
+import java.io.*;
+
+public class LearnJavaOOPs {
+    //OOPs Intro
+    // public static class Person {
+    //     int age;
+    //     String name;
+
+    //     void saysHi(){
+    //         System.out.println(name + " is " + age + " Years old, Says Hi!");
+    //     }
+        
+    // }
+
+    // public static void main(String[] args) {
+    //     Person p1 = new Person();
+    //     p1.age = 14;
+    //     p1.name = "Mohit";
+    //     p1.saysHi();
+
+    //     Person p2 = new Person();
+    //     p2.age = 16;
+    //     p2.name = "Surbhi";
+    //     p2.saysHi();
+    // }
+
+
+
+
+
+}
