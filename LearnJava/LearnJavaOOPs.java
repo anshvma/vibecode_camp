8d17ab7f94f2bbd12bb55de5a5cef7190de25675
Ansh varma <122360981+anshvrma@users.noreply.github.com>
2026-02-04 18:02:38 +0530

    SwapPerson

--- LearnJava/LearnJavaOOPs.java
+++ LearnJava/LearnJavaOOPs.java
@@ -5,15 +5,15 @@ import java.io.*;
 
 public class LearnJavaOOPs {
     //OOPs Intro
-    // public static class Person {
-    //     int age;
-    //     String name;
+    public static class Person {
+        int age;
+        String name;
 
-    //     void saysHi(){
-    //         System.out.println(name + " is " + age + " Years old, Says Hi!");
-    //     }
+        void saysHi(){
+            System.out.println(name + " is " + age + " Years old, Says Hi!");
+        }
         
-    // }
+    }
 
     // public static void main(String[] args) {
     //     Person p1 = new Person();
@@ -27,6 +27,34 @@ public class LearnJavaOOPs {
     //     p2.saysHi();
     // }
 
+    // Swap game
+    // public static void main(String[] args) {
+    //     Person p1 = new Person();
+    //     p1.age = 14;
+    //     p1.name = "Anit";
+        
+    //     Person p2 = new Person();
+    //     p2.age = 16;
+    //     p2.name = "Bonni";
+
+    //     p1.saysHi();
+    //     p2.saysHi();
+    //     swap1(p1, p2);
+    //     p1.saysHi();
+    //     p2.saysHi();
+    // }
+
+    // public static void swap1(Person prsn1, Person prsn2){
+    //     int age = prsn1.age;
+    //     prsn1.age = prsn2.age;
+    //     prsn2.age = age;
+
+    //     String name = prsn1.name;
+    //     prsn1.name = prsn2.name;
+    //     prsn2.name = name;
+    // }
+
+    
 
 
 
