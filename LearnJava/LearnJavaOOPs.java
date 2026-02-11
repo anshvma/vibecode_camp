2986523b5d2785b7d845f902bf168269515c0dc6
Ansh varma <122360981+anshvrma@users.noreply.github.com>
2026-02-11 15:30:37 +0530

    QueueIntro

--- LearnJava/LearnJavaOOPs.java
+++ LearnJava/LearnJavaOOPs.java
@@ -417,7 +417,21 @@ public class LearnJavaOOPs {
     //     }
     // }
 
-    
+    // Queue Introduction
+    public static void main(String[] args) {
+        Queue<Integer> que = new ArrayDeque<>();
+
+        que.add(10);
+        que.add(20);
+        que.add(30);
+        System.out.println(que);
+
+        System.out.println(que.peek());
+        System.out.println(que);
+
+        System.out.println(que.remove());
+        System.out.println(que);        
+    }
 
 
 
