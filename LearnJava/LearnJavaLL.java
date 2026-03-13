0811c1692b0cd026b76b1f2a90e1af645b50dba5
Ansh <anshverma110044@gmail.com>
2026-03-13 15:03:32 +0530

    QueueToStackAdapter -Push efficient

--- LearnJava/LearnJavaLL.java
+++ LearnJava/LearnJavaLL.java
@@ -791,8 +791,90 @@ public class LearnJavaLL {
     //     }
     // }
 
-    
+    // Queue to Stack Adapter - push efficient
+    public static class QueueToStackAdapterPS {
+        Queue<Integer> mainQ;
+        Queue<Integer> helperQ;
 
+        public QueueToStackAdapterPS(){
+            mainQ = new ArrayDeque<>();
+            helperQ = new ArrayDeque<>();
+        }
+
+        int size() {
+            return mainQ.size();
+        }
+
+        void push(int val){
+            mainQ.add(val);
+        }
+
+        int pop() {
+            if(size() == 0){
+                System.out.println("Stack Underflow");
+                return -1;
+            } else{
+                while(mainQ.size() > 1){
+                    helperQ.add(mainQ.remove());
+                }
+                int val = mainQ.remove();
+
+                while(helperQ.size() > 0){
+                    mainQ.add(helperQ.remove());
+                }
+                return val;
+            }
+        }
+
+        int top() {
+            if(size() == 0){
+                System.out.println("Stack Underflow");
+                return -1;
+            } else{
+                while(mainQ.size() > 1){
+                    helperQ.add(mainQ.remove());
+                }
+                int val = mainQ.remove();
+                helperQ.add(val);
+
+                while(helperQ.size() > 0){
+                    mainQ.add(helperQ.remove());
+                }
+                return val;
+            }
+        }
+        
+    }
+
+    // public static void main(String[] args) throws Exception {
+    //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //     QueueToStackAdapterPS st = new QueueToStackAdapterPS();
+
+    //     String str = br.readLine();
+    //     while(str.equals("quit") == false){
+    //         if(str.startsWith("push")){
+    //             int val = Integer.parseInt(str.split(" ")[1]);
+    //             st.push(val);
+    //         } else if(str.startsWith("pop")){
+    //             int val = st.pop();
+    //             if(val != -1){
+    //                 System.out.println(val);
+    //             }
+    //         } else if(str.startsWith("top")){
+    //             int val = st.top();
+    //             if(val != -1){
+    //                 System.out.println(val);
+    //             }
+    //         } else if(str.startsWith("size")){
+    //             System.out.println(st.size());
+    //         } else{
+    //             System.out.println("Invalid arguments");
+    //         }
+    //         str = br.readLine();
+    //     }
+    // }
+
+    
 
 
 
