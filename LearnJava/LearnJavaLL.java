ce454b4a2be5ac7eca9731a40212b79cb373a989
Ansh <anshverma110044@gmail.com>
2026-03-17 16:04:53 +0530

    TwoStackArray

--- LearnJava/LearnJavaLL.java
+++ LearnJava/LearnJavaLL.java
@@ -1012,7 +1012,116 @@ public class LearnJavaLL {
     //     }
     // }
 
-    
+    //Two Stack in an Array
+    public static class TwoStack {
+        int[] data;
+        int tos1;
+        int tos2;
+
+        public TwoStack(int cap) {
+            data = new int[cap];
+            tos1 = -1;
+            tos2 = data.length;
+        }
+
+        int size1() {
+            return tos1 + 1;
+        }
+
+        int size2() {
+            return data.length - tos2;
+        }
+
+        void push1(int val) {
+            if(tos2 == tos1 + 1){
+                System.out.println("Stack Overflow");
+            } else {
+                tos1++;
+                data[tos1] = val;
+            }
+        }
+        void push2(int val) {
+            if(tos2 == tos1 + 1){
+                System.out.println("Stack Overflow");
+            } else {
+                tos2--;
+                data[tos2] = val;
+            }
+        }
+
+        int pop1(){
+            if(size1() == 0){
+                System.out.println("Stack Underflow");
+                return -1;
+            } else {
+                int val = data[tos1];
+                tos1--;
+                return val;
+            }
+        }
+        int pop2(){
+            if(size2() == 0){
+                System.out.println("Stack Underflow");
+                return -1;
+            } else {
+                int val = data[tos2];
+                tos2++;
+                return val;
+            }
+        }
+        int top1() {
+            if(size1() == 0){
+                System.out.println("Stack Underflow");
+                return -1;
+            } else {
+                int val = data[tos1];
+                return val;
+            }
+        }
+        int top2() {
+            if(size2() == 0){
+                System.out.println("Stack Underflow");
+                return -1;
+            } else {
+                int val = data[tos2];
+                return val;
+            }
+        }
+    }
+
+    // public static void main(String[] args) throws Exception {
+    //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //     int n = Integer.parseInt(br.readLine());
+    //     TwoStack st = new TwoStack(n);
+
+    //     String str = br.readLine();
+    //     while(str.equals("quit") == false){
+    //         if(str.startsWith("p1")){
+    //             int val = Integer.parseInt(str.split(" ")[1]);
+    //             st.push1(val);
+    //         } else if(str.startsWith("p2")){
+    //             int val = Integer.parseInt(str.split(" ")[1]);
+    //             st.push2(val);
+    //         } else if(str.startsWith("pop1")){
+    //             st.pop1();
+    //         } else if(str.startsWith("pop2")){
+    //             st.pop2();
+    //         } else if(str.startsWith("top1")){
+    //             int val = st.top1();
+    //             System.out.println(val);
+    //         } else if(str.startsWith("top2")){
+    //             int val = st.top2();
+    //             System.out.println(val);
+    //         } else if(str.startsWith("size1")){
+    //             int val = st.size1();
+    //             System.out.println(val);
+    //         } else if(str.startsWith("size2")){
+    //             int val = st.size2();
+    //             System.out.println(val);
+    //         }
+    //         str = br.readLine();
+    //     }
+    // }
 
 
 
