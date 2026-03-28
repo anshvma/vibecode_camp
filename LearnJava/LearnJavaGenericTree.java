5a80b4877eb88586b44efd3c5ff737267e020bb8
Ansh <anshverma110044@gmail.com>
2026-03-28 15:40:07 +0530

    MaximumInGTree

--- LearnJava/LearnJavaGenericTree.java
+++ LearnJava/LearnJavaGenericTree.java
@@ -35,6 +35,38 @@ public class LearnJavaGenericTree {
         return s + 1;
     }
 
+    public static int max(Node node){
+        int m = Integer.MIN_VALUE;
+        for(Node child: node.children){
+            int cm = max(child);
+            m = Math.max(m, cm);
+        }
+        m = Math.max(m, node.data);
+        return m;
+    }
+
+    public static Node construct(int[] arr){
+        Node root = null;
+        Stack<Node> st = new Stack<>();
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == -1){
+                st.pop();
+            } else {
+                Node t = new Node();
+                t.data = arr[i];
+
+                if(st.size() > 0){
+                    st.peek().children.add(t);
+                } else {
+                    root = t;
+                }
+
+                st.push(t);
+            }
+        }
+        return root;
+    }
+
     // public static void main(String[] args) throws Exception {
     //     int[] arr = {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1};
             
@@ -60,6 +92,19 @@ public class LearnJavaGenericTree {
     //     System.out.println(size(root));
     // }
 
-    
+    // public  static void main(String[] args) throws Exception {
+    //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //     int n = Integer.parseInt(br.readLine());
+    //     int[] arr = new int[n];
+    //     String[] values = br.readLine().split(" ");
+    //     for (int i = 0; i < n; i++) {
+    //         arr[i] = Integer.parseInt(values[i]);
+    //     }
 
+    //     Node root = construct(arr);
+    //     int m = max(root);
+    //     System.out.println(m);
+    // }
+
+    
 }
