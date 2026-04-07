98de7901a198232613b00c952653394f4b3a0819
Ansh <anshverma110044@gmail.com>
2026-04-07 13:59:28 +0530

    removesleavesfromGTree

--- LearnJava/LearnJavaGenericTree.java
+++ LearnJava/LearnJavaGenericTree.java
@@ -163,6 +163,18 @@ public class LearnJavaGenericTree {
          Collections.reverse(node.children);
      }
 
+    public static void removeLeaves(Node node){
+         for(int i = node.children.size() - 1; i >= 0; i--){
+             Node child = node.children.get(i);
+             if(child.children.size() == 0){
+                 node.children.remove(i);
+             }
+         }
+
+         for(Node child: node.children){
+             removeLeaves(child);
+         }
+     }
 
     // public static void main(String[] args) throws Exception {
     //     int[] arr = {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1};
@@ -289,8 +301,22 @@ public class LearnJavaGenericTree {
     //     display(root);
     //  }
 
-    
+    //remove leaves of a generic tree
+    //  public  static void main(String[] args) throws Exception {
+    //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //     int n = Integer.parseInt(br.readLine());
+    //     int[] arr = new int[n];
+    //     String[] values = br.readLine().split(" ");
+    //     for (int i = 0; i < n; i++) {
+    //         arr[i] = Integer.parseInt(values[i]);
+    //     }
 
+    //     Node root = construct(arr);
+    //     removeLeaves(root);
+    //     display(root);
+    //  }
+
+    
 
 
 
